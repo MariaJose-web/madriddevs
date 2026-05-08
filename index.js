311bd67db133f781f212eaec671000922361571b
mjhervasmozos26 <mjhervasmozos26@gmail.com>
2026-05-08 12:25:21 +0200

    trabajo de clase 08.05.2026

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Importamos express, 
 import express from "express";
+//Importamos el json. 
+import data from './data.json' with { type:"json" };
+//Generamos las variables funcionales. 
 const app = express(); 
 const port = 3000; 
-const data = {nombre:'Madrid claro que sí'};
+
 
 //Middleware Creamos la carpeta public para los elementos estaticos 
 app.use('/', express.static('public')); 
@@ -10,13 +13,29 @@ app.use('/', express.static('public'));
 //Configuramos ejs. para decir que es el motor de plantillas. 
 app.set('view engine', 'ejs') 
 //Definimos la carpeta que vamos a utilizar. si no se pone nada por defecto es views. 
-app.set('views', './views') //
+app.set('views', './views') 
 
 //Rutas
 app.get('/', (req, res) => {
     //res.send(`<h1>Hola Mundo</h1>`);
-    res.render('index', data); // renderiza la template con los datos para crear la pagina a mostrar 
+    res.render('index', {data}); // renderiza la template con los datos para crear la pagina a mostrar 
 } ) 
+// Dirección Dinamica para la lista de usuarios. 
+const mostrarLista = function (req, res) {
+    //res.json(data); 
+    res.render('lista.ejs', {data}); 
+ }
+app.get('/lista', mostrarLista); 
+
+//Dirección Dinamica para un solo dev(usuario)
+const mostrarUno = function (req, res) {
+    const {id} = req.params;  //DEvuelve un objeto en string
+    //const id = parseInt(req.params.id);   Generamos un campo nuevp accediendo a la variable que es un string y luego lo convertimos a un numerico.
+    const persona = data.find((item) => item.id==id);  
+    //res.send(persona); 
+    res.render('detalleDev.ejs', {persona});
+} 
+app.get('/lista/:id', mostrarUno); 
 
 //Lanzar Servidor
 app.listen(port, ()=>{console.log(`Has iniciado servicio en el puesto ${port}`)})
