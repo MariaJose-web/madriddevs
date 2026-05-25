fb7ec19f1b0bee4732894464524d5c5d9d29bcd1
mjhervasmozos26 <mjhervasmozos26@gmail.com>
2026-05-25 08:36:28 +0200

    Cambios 25.05.2026

--- index.js
+++ index.js
@@ -37,6 +37,11 @@ app.get('/lista', mostrarLista);
 } 
 app.get('/lista/:id', mostrarUno); 
 
+
+//middleware 404
+app.use((req, res, next) => {
+    res.status(404).send("404-Rescurso No Encontrado")  // tambien se puede poner un pag personalizada
+})
 //Lanzar Servidor
 app.listen(port, (error)=>{
     if (error) {console.log(error);};
